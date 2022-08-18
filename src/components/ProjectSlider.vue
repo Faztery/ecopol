44124921dda569478c252b230b18cce69bcfe62f
porrasmdz <aporrasmdz@gmail.com>
2022-08-18 14:49:37 -0500

    work

---
+++ src/components/ProjectSlider.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { reactive, computed, defineProps } from 'vue'
+const props = defineProps({
+    first: {
+        type: Boolean,
+        default: false
+    }
+}
+)
+
+
+</script>
+
+<template>
+    <!-- El primer FullScreen Div en cada View necesita usar -mt-0 pt-0 si el header es sticky -->
+    <div class="p-8 min-h-screen">
+        
+        <slot/>
+        
+    </div>
+</template>
